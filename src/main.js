4f8eba534e41d181f520866642fcbe80d09201d2
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2025-05-16 19:38:36 +0700

    added tech slides

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import "./style.css";
+
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+const swiper = new Swiper(".tech-stack-slider", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  loop: true,
+  allowTouchMove: false, // Disable manual swiping
+  autoplay: {
+    delay: 1, // Minimal delay
+    disableOnInteraction: false,
+    reverseDirection: true,
+  },
+  speed: 6000, // Adjust this value to control the sliding speed
+  slidesPerView: "auto",
+  freeMode: {
+    enabled: true,
+    momentum: false,
+  },
+});
