66ded49af5bdb51c3c654624cc0ecac890a14173
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2025-05-18 14:32:56 +0700

    sebelum polish tech stack

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 import "./style.css";
 
+import { html } from "code-tag";
 import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 
@@ -112,35 +113,33 @@ function renderWork() {
   workContainer.innerHTML = workExperiences
     .map(
       (workEx) =>
+        html`
+          <div
+            class="flex gap-9 p-6 rounded-xl shadow-xs justify-start items-start"
+          >
+            <div>
+              <img class="w-16" src="${workEx.img}" alt="" />
+            </div>
+            <div class="flex flex-col gap-2">
+              <h3 class="text-lg font-semibold">${workEx.role}</h3>
+              <p class="-mt-3 text-green-600">${workEx.place}</p>
+              <ul class="list-disc list-inside text-gray-600">
+                ${workEx.responsibilities
+                  .map((responsibility) => `<li>${responsibility}</li>`)
+                  .join("")}
+              </ul>
+              <ul class="flex gap-4 mt-2 text-gray-700 text-xs font-medium">
+                ${workEx.techUsed
+                  .map(
+                    (tech) =>
+                      `<li class="bg-gray-200 px-3 py-1 rounded-full">${tech}</li>`
+                  )
+                  .join("")}
+              </ul>
+            </div>
+            <p class="ms-auto text-gray-500">${workEx.date}</p>
+          </div>
         `
-    <div
-      class="flex gap-9 p-6 rounded-xl shadow-xs justify-start items-start"
-    >
-      <div>
-        <img class="w-16" src="${workEx.img}" alt="" />
-      </div>
-      <div class="flex flex-col gap-2">
-        <h3 class="text-lg font-semibold">
-          ${workEx.role}
-        </h3>
-        <p class="-mt-3 text-green-600">${workEx.place}</p>
-        <ul class="list-disc list-inside text-gray-600">
-          ${workEx.responsibilities
-            .map((responsibility) => `<li>${responsibility}</li>`)
-            .join("")}
-        </ul>
-        <ul class="flex gap-4 mt-2 text-gray-700 text-xs font-medium">
-          ${workEx.techUsed
-            .map(
-              (tech) =>
-                `<li class="bg-gray-200 px-3 py-1 rounded-full">${tech}</li>`
-            )
-            .join("")}
-        </ul>
-      </div>
-      <p class="ms-auto text-gray-500">${workEx.date}</p>
-    </div>
-    `
     )
     .join("");
 }
@@ -211,8 +210,8 @@ function renderProjects() {
   projectContainer.innerHTML = myProjects
     .map(
       (project) =>
-        `
-    <div class="flex flex-col gap-2 rounded-2xl shadow overflow-hidden">
+        html`
+          <div class="flex flex-col gap-2 rounded-2xl shadow overflow-hidden">
             <div class="h-52 bg-gray-100">
               <img
                 class="h-full mx-auto"
@@ -223,9 +222,7 @@ function renderProjects() {
 
             <div class="flex flex-col gap-4 m-9">
               <h3 class="text-xl font-semibold">${project.title}</h3>
-              <p>
-                ${project.decs}
-              </p>
+              <p>${project.decs}</p>
 
               <ul class="flex gap-6 text-gray-600 font-medium text-xs">
                 <li class="bg-gray-200 px-3 py-1 rounded-2xl">React</li>
@@ -240,20 +237,17 @@ function renderProjects() {
               </ul>
 
               <div class="flex gap-8">
-                
-                ${
-                  project.githubRepo
-                    ? `<div class="flex gap-1 text-blue-600 items-center">
+                ${project.githubRepo
+                  ? `<div class="flex gap-1 text-blue-600 items-center">
                   <svg class="w-4 h-4 mr-1.5" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.237 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
                 </svg>
                   <span>View on GitHub</span>
                 </div>`
-                    : `<div class="flex gap-1">
+                  : `<div class="flex gap-1">
                   <img src="./src/icons/icons8-github-logo.svg" alt="" />
                   <span>Private Repository</span>
-                </div>`
-                }
+                </div>`}
                 <div class="flex gap-1 items-center">
                   <img
                     class="w-4 h-4"
@@ -265,7 +259,7 @@ function renderProjects() {
               </div>
             </div>
           </div>
-    `
+        `
     )
     .join("");
 }
