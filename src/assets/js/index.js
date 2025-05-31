0bc266eb7f01c9d50a4ea88d3ffb42f0aec77013
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2025-05-31 22:54:45 +0700

    swiperjs refactor

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,24 +4,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Initialize Swiper
   const swiper = new Swiper(".swiper", {
-    direction: "horizontal",
-    loop: true,
-    slidesPerView: 9,
-    spaceBetween: 30,
+    direction: "horizontal", // 'horizontal' or 'vertical'
+    loop: true, // true -> enable continuous loop
+    slidesPerView: 9, // number of visible element per view
 
     // Smooth continuous movement
-    speed: 4000,
-    allowTouchMove: true,
+    speed: 4000, // duration of transition (ms)
+    allowTouchMove: true, // ability to grabbing it with mouse
     autoplay: {
-      delay: 1,
-      disableOnInteraction: false,
-      reverseDirection: true,
-    },
-
-    // Enable continuous smooth sliding
-    freeMode: {
-      enabled: true,
-      momentum: false,
+      delay: 1, // delay between init and slide moves
+      disableOnInteraction: false, // false -> after mouse grab it moves back
+      reverseDirection: true, // true -> left to right
     },
   });
 });
