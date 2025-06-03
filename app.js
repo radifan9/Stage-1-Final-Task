65438cbe27e63f90dc88859d78c8d02fd9a475c6
Radif Aghnadiin <radif.aghnadiin@gmail.com>
2025-06-03 20:35:03 +0700

    added login register

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ import path from "path";
 import TECH_STACKS from "./src/data/techstacks.js";
 import COMPANIES_LOGO from "./src/data/companyLogo.js";
 import PROJECT_IMAGE from "./src/data/projectDemo.js";
+import bcrypt from "bcrypt";
+import flash from "express-flash";
+import session from "express-session";
 
 // 2. Constants and Configuration
 const CONFIG = {
-  nodePort: 3000,
+  nodePort: 3001,
   database: {
     user: "postgres",
     password: "ms11drag00nsql",
@@ -30,7 +33,49 @@ app.set("view engine", "hbs");
 app.set("views", "src/views");
 app.use("/assets", express.static("src/assets"));
 app.use("/swiper", express.static("node_modules/swiper"));
+app.use(express.urlencoded({ extended: false })); // Parses form data
+app.use(
+  session({
+    secret: "ruby-chan",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false, // true = https
+      maxAge: 1000 * 60 * 60, // 1 hour
+    },
+  })
+);
+app.use(flash());
+// Add session to middleware, so we don't have to pass userData to every route
+app.use((req, res, next) => {
+  res.locals.userData = {
+    name: req.session.user?.name,
+    email: req.session.user?.email,
+  };
+  next();
+});
+
+// Create authentication middleware
+const requireAuth = (req, res, next) => {
+  console.log(`--- authentication`);
+  console.log(`--- req.session.user: ${req.session.user}`);
+  if (req.session.user) {
+    // User is authenticated, proceed
+    return next();
+  } else {
+    // User is not authenticated
+    req.flash("error", "⚠️ Please log in to access dashboard");
+    return res.redirect("/login");
+  }
+};
+
 hbs.registerPartials(path.join(__dirname, "src/views/partials"));
+
+// --------- hbs helper
+// Equality comparison, return true if equal
+hbs.registerHelper("eq", function (a, b) {
+  return a === b;
+});
 
 // 5. Utility functions
 // Get techStacks title and img from contant TECH_STACKS
@@ -133,8 +178,104 @@ const renderIndex = async (req, res) => {
   }
 };
 
+const renderRegister = (req, res) => {
+  res.render("register", {
+    title: "Register",
+    message: {
+      error: req.flash("error"),
+    },
+  });
+};
+
+const handleRegister = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // Check if email already exists
+    const { rows: isRegistered } = await db.query({
+      text: "SELECT email FROM public.users WHERE email=$1",
+      values: [email],
+    });
+    if (isRegistered.length > 0) {
+      req.flash("error", "⚠️ Email already registered");
+      return res.redirect("/register");
+    }
+
+    // Hash password & create user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await db.query({
+      text: "INSERT INTO public.users (name, email, password) VALUES ($1, $2, $3)",
+      values: [name, email, hashedPassword],
+    });
+    req.flash("success", "✅ Registration successful! Please login.");
+    res.redirect("/login");
+  } catch (error) {
+    console.log("Registration error:", error);
+    // req.flash("error", "⚠️ An error occurred during registration");
+    res.redirect("/register");
+  }
+};
+
+const renderLogin = (req, res) => {
+  res.render("login", {
+    title: "Login",
+    message: {
+      success: req.flash("success"),
+      error: req.flash("error"),
+    },
+  });
+};
+
+const handleLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const { rows: isRegistered } = await db.query({
+      text: "SELECT * FROM public.users WHERE email = $1",
+      values: [email],
+    });
+
+    console.log({ email, password });
+    console.log(`Hashed password: ${isRegistered.at(0).password}`);
+
+    // console.log({ isRegistered });
+    if (!isRegistered.at(0)) {
+      req.flash("error", "⚠️ User not found");
+      return res.redirect("/login");
+    }
+
+    // Verify password
+    const isMatch = await bcrypt.compare(password, isRegistered.at(0).password);
+    if (!isMatch) {
+      // If password isn't match
+      req.flash("error", "⚠️ Incorrect password");
+      return res.redirect("/login");
+    }
+
+    // Set session and redirect
+    req.session.user = {
+      name: isRegistered.at(0).name,
+      email: isRegistered.at(0).email,
+    };
+    res.redirect("/dashboard");
+  } catch (error) {
+    console.log("Login error:", error);
+    req.flash("error", "⚠️ An error occurred during login");
+    res.redirect("/login");
+  }
+};
+
+const renderDashboard = (req, res) => {
+  res.render("dashboard", {
+    title: "dashboard",
+    path: "/dashboard",
+  });
+};
+
 // 7. Routes
 app.route("/").get(renderIndex);
+app.route("/register").get(renderRegister).post(handleRegister);
+app.route("/login").get(renderLogin).post(handleLogin);
+app.route("/dashboard").get(requireAuth, renderDashboard);
 
 // 8. Server start
 app.listen(CONFIG.nodePort, () => {
